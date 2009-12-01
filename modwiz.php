a5b25e28d0db341e83f1bcab73db80cd9f9995bc
Standa Opichal <opichals@gmail.com>
2009-12-01 21:18:39 +0100

    An empty $exclude array variable doesn't filter out all files.

--- modwiz.php
+++ modwiz.php
@@ -3,20 +3,27 @@ header('Content-Type: text/xml');
 
 require 'files.php';
 
-if (is_array($exclude)) {
+if (is_array($exclude) && count($exclude) > 0) {
     $exclude = '('.join('|', $exclude).'|^\s+$)';
+} else {
+    $exclude = '';
 }
 # echo '<!-- Excluding: '.$exclude." -->\n";
 
-$farray = array_unique( preg_split("/[\n\r\s]+/", $files) );
-$farray = preg_grep("/{$exclude}/i", $farray, PREG_GREP_INVERT);
+$farray = array_unique( preg_split("/[\n\r]+/", $files) );
+if ($exclude != '') {
+    $farray = preg_grep("/{$exclude}/i", $farray, PREG_GREP_INVERT);
+}
 
 # get rid of the last, empty string ??
 array_pop($farray);
 
 # by default we expect the files to come from the folder above ours
-function dotdot($f) { return preg_replace('/^/', '../', $f); }
-$farray = array_map(dotdot, $farray);
+function dotdot($f) {
+    $f = trim($f);
+    return preg_replace('/^/', '../', $f);
+}
+$farray = array_map('dotdot', $farray);
 
 # build the Rhino file list to go through
 $files = join(" ", $farray);
